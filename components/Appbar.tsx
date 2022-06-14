8d43e73750d4d8f725ff14bc7d5205e2107bfd6d
khream42 <khream42@proton.me>
2022-06-14 03:49:32 -0400

    ok

--- components/Appbar.tsx
+++ components/Appbar.tsx
@@ -7,21 +7,21 @@ function Appbar() {
         fluid={true}
         rounded={true}
       >
-        <Navbar.Brand href="https://flowbite.com/">
+        <Navbar.Brand href="https://khream42.github.io/">
           <img
-            src="https://flowbite.com/docs/images/logo.svg"
+            src="/cream.png"
             className="mr-3 h-6 sm:h-9"
             alt="Flowbite Logo"
           />
           <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
-            Flowbite
+            khream
           </span>
         </Navbar.Brand>
         <div className="flex md:order-2">
           <Dropdown
             arrowIcon={false}
             inline={true}
-            label={<Avatar alt="User settings" img="https://flowbite.com/docs/images/people/profile-picture-5.jpg" rounded={true} />}
+            label={<Avatar alt="User settings" img="/profile.jpg" rounded={true} />}
           >
             <Dropdown.Header>
               <span className="block text-sm">
