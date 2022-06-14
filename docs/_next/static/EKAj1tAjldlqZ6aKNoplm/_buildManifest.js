587e46a26758046986284ee037c758d157f74852
khream42 <khream42@proton.me>
2022-06-14 03:50:33 -0400

    out

--- docs/_next/static/EKAj1tAjldlqZ6aKNoplm/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/d64684d8-125749b6fb22f688.js","static/chunks/944-57a222d92d63f78c.js","static/chunks/pages/index-2e2a20c28bf5c11e.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
